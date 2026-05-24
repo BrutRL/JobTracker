6a945e00941905b4ad5ed47fb49171a40b4681f7
BrutRL <ar7459101@gmail.com>
2026-05-24 18:48:12 +0800

    with dns config ivp4

--- backend/middleware/nodemailer.js
+++ backend/middleware/nodemailer.js
@@ -1,12 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config();
 import nodemailer from "nodemailer";
+import dns from "dns";
+
+// force IPv4 globally
+dns.setDefaultResultOrder("ipv4first");
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  family: 4, // force IPv4 — fixes Render IPv6 issue
 });
